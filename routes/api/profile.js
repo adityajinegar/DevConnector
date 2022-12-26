3d3de29129d85d51bb259f0a00c8adf9abe8d2b8
Aditya <aditya.jinegar@mmhg.ca>
2022-12-25 20:30:35 -0500

    profile field validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const mnongoose = require('mongoose');
 const passport = require('passport');
 const router = express.Router();
 
+// Load validation
+const validateProfileInput = require('../../validation/profile');
+
 // Load Profile model
 const Profile = require('../../models/Profile');
 
@@ -24,6 +27,7 @@ router.get(
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
+      .populate('user', ['name', 'avatar'])
       .then((profile) => {
         if (!profile) {
           errors.noprofile = 'There is no profile for this user';
@@ -42,6 +46,14 @@ router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    //. Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
     // Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
